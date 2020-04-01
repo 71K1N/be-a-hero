a5eac3aba42890c3cb9a6686c79e301fe327b9eb
Luiz Carlos <d71k1n@live.com>
2020-04-01 10:13:58 -0300

    Font-end do app

---
+++ frontend/src/pages/Register/index.js
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import { Link , useHistory} from "react-router-dom";
+import { FiArrowLeft } from "react-icons/fi";
+import api from "../../services/api";
+
+import "./style.css";
+
+import logoImg from "../../assets/logo.svg";
+
+export default function Register() {
+  //  - CAMPOS DO FORMULARIO
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [watsapp, setWatsapp] = useState("");
+  const [city, setCity] = useState("");
+  const [uf, setUf] = useState("");
+
+  const history = useHistory();
+
+  async function handleRegister(e) {
+    e.preventDefault();
+    const data = {
+      name,
+      email,
+      watsapp,
+      city,
+      uf
+    };
+    try {
+      const response = await api.post("ongs", data);
+      alert(` Seu ID ${response.data.id}`);
+      history.push('/');
+    } catch (error) {
+      alert("erro na cadastro, tente novamente.");
+    }
+  }
+  return (
+    <div className="register-container">
+      <div className="content">
+        <section>
+          <img src={logoImg} alt="be the hero" />
+          <h1> Cadastro </h1>
+          <p>
+            Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem
+            os casos da sua ONG.
+          </p>
+          <Link to="/" className="back-link">
+            <FiArrowLeft size={16} color="E02042" />
+            Não tenho cadastro
+          </Link>
+        </section>
+        <form onSubmit={handleRegister}>
+          <input
+            placeholder="Nome da ONG"
+            value={name}
+            onChange={e => setName(e.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+          <input
+            placeholder="Watsapp"
+            value={watsapp}
+            onChange={e => setWatsapp(e.target.value)}
+          />
+          <div className="input-group">
+            <input
+              placeholder="Cidade"
+              value={city}
+              onChange={e => setCity(e.target.value)}
+            />
+            <input
+              placeholder="Uf"
+              style={{
+                width: 80
+              }}
+              value={uf}
+              onChange={e => setUf(e.target.value)}
+            />
+          </div>
+          <button className="button" type="submit">
+            Cadastrar
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
